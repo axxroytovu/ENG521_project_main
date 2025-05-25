9cb1a0b905c722be7caf1d6807480e3f252d8dae
Graham <grahampg@gmail.com>
2025-05-24 22:28:47 -0700

    Add autograd features for heat equation

--- deep_learning/utils.py
+++ deep_learning/utils.py
@@ -37,3 +37,26 @@ def grad(outputs, inputs):
         grad_outputs=torch.ones_like(outputs),
         create_graph=True
     )
+
+# general laplacian isn't currently working
+def laplacian(y, x):
+    lap = torch.zeros_like(y)
+    for xi in x:
+        dy_dxi = autograd(y, xi, torch.ones_like(y), create_graph=True, allow_unused=True)[0]
+        lap += autograd(dy_dxi, xi, torch.ones_like(y), create_graph=True)[0]
+    return lap
+
+def laplacian_1d(y, x):
+    dy_dx = autograd(y, x, torch.ones_like(y), create_graph=True)[0]
+    return autograd(dy_dx, x, torch.ones_like(dy_dx), create_graph=True)[0]
+
+def laplacian_2d(z, x, y):
+    lap = torch.zeros_like(z)
+    dz_dx = autograd(z, x, torch.ones_like(z), create_graph=True)[0]
+    lap += autograd(dz_dx, x, torch.ones_like(dz_dx), create_graph=True)[0]
+    dz_dy = autograd(z, y, torch.ones_like(z), create_graph=True)[0]
+    lap += autograd(dz_dy, y, torch.ones_like(dz_dy), create_graph=True)[0]
+    return lap
+
+def dy_dt(y, t):
+    return autograd(y, t, torch.ones_like(y), create_graph=True)[0]
